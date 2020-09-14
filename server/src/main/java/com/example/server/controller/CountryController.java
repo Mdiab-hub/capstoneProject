52c2ab07c3886c56eedc16b690166fa69626bee1
Mdiab-hub <Mdiab-hub>
2020-09-14 02:37:36 -0400

    backend

--- server/src/main/java/com/example/server/controller/CountryController.java
+++ server/src/main/java/com/example/server/controller/CountryController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/countries")
 public class CountryController {
     @Autowired
